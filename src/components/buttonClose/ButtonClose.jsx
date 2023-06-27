e05516cd248e3e1e9d263ca8e0bd2653c2eb38e3
Alexandr Kotenko <kpshmt@gmail.com>
2023-06-27 14:37:22 +0300

    update

--- src/components/buttonClose/ButtonClose.jsx
+++ src/components/buttonClose/ButtonClose.jsx
@@ -5,7 +5,7 @@ import styles from './ButtonClose.module.scss';
 function ButtonClose({ variant = 'closeBtn', type = 'button', onClick }) {
   return (
     <button type={type} className={styles[variant]} onClick={onClick}>
-      <GrFormClose size={40} />
+      <GrFormClose size={20} />
     </button>
   );
 }
